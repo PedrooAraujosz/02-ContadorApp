b4ebe432153b9c4e1b024f7cefff6fa47d9e288b
PEDRO ARAUJO <mxpedro.araujo11@gmail.com>
2025-08-26 16:59:13 -0300

    Add counter functionality with increment, decrement, and reset features

--- App.js
+++ App.js
@@ -1,10 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function App() {
+  //contador vai comçar no 0
+  const [contador, setContador] = useState(0);
+
+  const handleIncrement = () => {
+    setContador(contador + 1);
+  };
+
+  const handleDecrement = () => {
+    contador > 0 && setContador(contador - 1);
+  };
+
+  const handleReset = () => {
+    setContador(0);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Contador</Text>
+      <Text style={styles.counterText}>{contador}</Text>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button} onPress={handleIncrement}>
+          <Text>Aumentar</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button} onPress={handleDecrement}>
+          <Text>Diminuir</Text>
+        </TouchableOpacity>
+      </View>
+      <TouchableOpacity
+        style={[styles.button, styles.resetButton]}
+        onPress={handleReset}
+      >
+        <Text style={styles.buttonText}>Zerar</Text>
+      </TouchableOpacity>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +46,39 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 32,
+    fonweight: "bold",
+    color: "#333",
+    marginBottom: 20,
+  },
+  counterText: {
+    fontSize: 64,
+    fonweight: "bold",
+    color: "#333",
+    marginBottom: 30,
+  },
+  button: {
+    backgroundColor: "#007bff",
+    paddingVertical: 15,
+    paddingHorizontal: 40,
+    borderRadius: 10,
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  buttonContainer: {
+    flexDirection: "row-reverse",
+    marginBottom: 20,
+    gap: 6,
+  },
+  resetButton: {
+    backgroundColor: "#dc3545",
   },
 });
